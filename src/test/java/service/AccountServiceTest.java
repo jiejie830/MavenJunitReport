dca3a8f8ae09a0e21cccc7b9479bad70436eb874
jiejie830 <zhangjie830@139.com>
2017-04-04 20:42:53 +0800

    add trans
    add trans

--- src/test/java/service/AccountServiceTest.java
+++ src/test/java/service/AccountServiceTest.java
@@ -1,20 +1,19 @@
 package service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import domain.Account;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:service/Spring-db1.xml")
-@Transactional
+
 public class AccountServiceTest {
 	
 
@@ -23,9 +22,10 @@ public class AccountServiceTest {
 
 	@Test
 	public void testGetAcccountById() {
-		Account acct2 = Account.getAccount(2, "user02", 18, "F");
+		Account acct2 = Account.getAccount(3, "user03", 18, "F");
 
 		accountService.saveAccount(acct2);
+		
 		Account acct1 = accountService.getAccountById(2);
 		assertEquals(acct2.getId(), acct1.getId());
 	}
